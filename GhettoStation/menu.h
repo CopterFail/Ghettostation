752778bf1ae65733e1778292f858b9c743705e79
CopterFail <dabrowski@web.de>
2015-04-24 22:19:56 +0200

    format

--- GhettoStation/menu.h
+++ GhettoStation/menu.h
@@ -8,7 +8,6 @@
 #ifndef MENU_H_
 #define MENU_H_
 
-
 /*########################################### MENU ##################################################*/
 MenuSystem displaymenu;
 Menu rootMenu("");
@@ -16,28 +15,24 @@ MenuItem m1i1Item("START");
 MenuItem m1i2Item("SET HOME");
 Menu m1m3Menu("CONFIG");
 	Menu m1m3m1Menu("SERVOS");
-            Menu m1m3m1m1Menu("PAN");
-                MenuItem m1m3m1m1l1Item("MINPWM");
-                MenuItem m1m3m1m1l2Item("MAXPWM");
-                MenuItem m1m3m1m1l3Item("MINANGLE");
-                MenuItem m1m3m1m1l4Item("MAXANGLE");
-	    Menu m1m3m1m2Menu("TILT");
-                MenuItem m1m3m1m2l1Item("MINPWM");
-                MenuItem m1m3m1m2l2Item("MAXPWM");
-                MenuItem m1m3m1m2l3Item("MINANGLE");
-                MenuItem m1m3m1m2l4Item("MAXANGLE");
-            MenuItem m1m3m1i3Item("TEST");
-        Menu m1m3m2Menu("TELEMETRY");
-            MenuItem m1m3m2i1Item("PROTOCOL");
-            MenuItem m1m3m2i2Item("BAUDRATE");
-        Menu m1m3m3Menu("OTHERS");
-            MenuItem m1m3m3i1Item("OSD");
-            MenuItem m1m3m3i2Item("BEARING METHOD");
-            MenuItem m1m3m3i3Item("BATTERY ALERT");
+		Menu m1m3m1m1Menu("PAN");
+			MenuItem m1m3m1m1l1Item("MINPWM");
+			MenuItem m1m3m1m1l2Item("MAXPWM");
+			MenuItem m1m3m1m1l3Item("MINANGLE");
+			MenuItem m1m3m1m1l4Item("MAXANGLE");
+		Menu m1m3m1m2Menu("TILT");
+			MenuItem m1m3m1m2l1Item("MINPWM");
+			MenuItem m1m3m1m2l2Item("MAXPWM");
+			MenuItem m1m3m1m2l3Item("MINANGLE");
+			MenuItem m1m3m1m2l4Item("MAXANGLE");
+		MenuItem m1m3m1i3Item("TEST");
+	Menu m1m3m2Menu("TELEMETRY");
+		MenuItem m1m3m2i1Item("PROTOCOL");
+		MenuItem m1m3m2i2Item("BAUDRATE");
+	Menu m1m3m3Menu("OTHERS");
+		MenuItem m1m3m3i1Item("OSD");
+		MenuItem m1m3m3i2Item("BEARING METHOD");
+		MenuItem m1m3m3i3Item("BATTERY ALERT");
 MenuItem m1i4Item("SWITCH SETTINGS");
-
-
-
-
 
 #endif /* MENU_H_ */
